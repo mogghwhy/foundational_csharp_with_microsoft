06fcd83405637e045322c6984677baec9b0ca58f
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-09-25 15:41:25 +0300

    complete the challenge

--- part01/p01_module02/Program.cs
+++ part01/p01_module02/Program.cs
@@ -1,1 +1,4 @@
-﻿var message;
+﻿var name = "Bob";
+var count = 3;
+var temperature = 34.4;
+Console.WriteLine("Hello, " + name + "! You have " + count + " messages in your inbox. The temperature is " + temperature + " celsius.");
