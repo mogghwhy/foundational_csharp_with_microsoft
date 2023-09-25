cf64a7bec83c7a0c41508b8e19261ec82405045b
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-09-25 10:53:25 +0300

    code for the challenge

--- part01_module01/Program.cs
+++ part01_module01/Program.cs
@@ -1,4 +1,2 @@
-﻿// Console.WriteLine("Hello World!");
-Console.Write("Congratulations!");
-Console.Write(" ");
-Console.Write("You wrote your first lines of code.");
+﻿Console.WriteLine("This is the first line.");
+Console.Write("This is the second line.!");
