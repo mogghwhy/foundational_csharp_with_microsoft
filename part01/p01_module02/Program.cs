f20475170752c1f5c0efc84584f8163d4b059346
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-09-25 15:36:01 +0300

    Reassign the value of a variable

--- part01/p01_module02/Program.cs
+++ part01/p01_module02/Program.cs
@@ -1,3 +1,9 @@
 ﻿string firstName;
 firstName = "Bob";
 Console.WriteLine(firstName);
+firstName = "Liem";
+Console.WriteLine(firstName);
+firstName = "Isabella";
+Console.WriteLine(firstName);
+firstName = "Yasmin";
+Console.WriteLine(firstName);
