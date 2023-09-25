c65e378ec173b786408775d94dba247118c336aa
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-09-25 15:29:12 +0300

    declare a string variable

--- part01/p01_module02/Program.cs
+++ part01/p01_module02/Program.cs
@@ -1,7 +1,8 @@
-﻿Console.WriteLine("Hello World!");
-Console.WriteLine(123);
-Console.WriteLine(0.25F);
-Console.WriteLine(2.625);
-Console.WriteLine(12.39816m);
-Console.WriteLine(true);
-Console.WriteLine(false);
+﻿// Console.WriteLine("Hello World!");
+// Console.WriteLine(123);
+// Console.WriteLine(0.25F);
+// Console.WriteLine(2.625);
+// Console.WriteLine(12.39816m);
+// Console.WriteLine(true);
+// Console.WriteLine(false);
+string firstName;
