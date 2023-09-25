19bcc8ebfde7e39697bae71158d8b7a2180c88d5
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-09-25 15:34:05 +0300

    defina a string variable and assign value to it

--- part01/p01_module02/Program.cs
+++ part01/p01_module02/Program.cs
@@ -1,13 +1,2 @@
-﻿// Console.WriteLine("Hello World!");
-// Console.WriteLine(123);
-// Console.WriteLine(0.25F);
-// Console.WriteLine(2.625);
-// Console.WriteLine(12.39816m);
-// Console.WriteLine(true);
-// Console.WriteLine(false);
-string firstName;
-
-char userOption;
-int gameScore;
-decimal particlesPerMillion;
-bool processedCustomer;
+﻿string firstName;
+firstName = "Bob";
