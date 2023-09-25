c322003f21f06fc9a7517d3d58d76b3145743a69
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-09-25 15:36:17 +0300

    Initialize the variable

--- part01/p01_module02/Program.cs
+++ part01/p01_module02/Program.cs
@@ -1,9 +1,2 @@
 ﻿string firstName;
-firstName = "Bob";
-Console.WriteLine(firstName);
-firstName = "Liem";
-Console.WriteLine(firstName);
-firstName = "Isabella";
-Console.WriteLine(firstName);
-firstName = "Yasmin";
 Console.WriteLine(firstName);
