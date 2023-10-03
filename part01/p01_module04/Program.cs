6c682eec2f1ce7efce0334a12f6dc2723a3b4035
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-10-03 11:21:58 +0300

    p1m4 - Write code to exercise C#'s order of operations

--- part01/p01_module04/Program.cs
+++ part01/p01_module04/Program.cs
@@ -1,2 +1,4 @@
-﻿Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
-Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
+﻿int value1 = 3 + 4 * 5;
+int value2 = (3 + 4) * 5;
+Console.WriteLine(value1);
+Console.WriteLine(value2);
