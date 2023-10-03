3c5f782ac2d807fc04c78572c9fc4dfe945b49a8
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-10-03 11:24:22 +0300

    p1m4 - Use the increment operator before and after the value

--- part01/p01_module04/Program.cs
+++ part01/p01_module04/Program.cs
@@ -1,19 +1,6 @@
 ﻿int value = 1;
-
-value = value + 1;
-Console.WriteLine("First increment: " + value);
-
-value += 1;
-Console.WriteLine("Second increment: " + value);
-
 value++;
-Console.WriteLine("Third increment: " + value);
-
-value = value - 1;
-Console.WriteLine("First decrement: " + value);
-
-value -= 1;
-Console.WriteLine("Second decrement: " + value);
-
-value--;
-Console.WriteLine("Third decrement: " + value);
+Console.WriteLine("First: " + value);
+Console.WriteLine($"Second: {value++}");
+Console.WriteLine("Third: " + value);
+Console.WriteLine("Fourth: " + (++value));
