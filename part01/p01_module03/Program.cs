68133613bace6f6d9119e38bbe71aabae5f1240e
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-10-03 10:23:17 +0300

    p1m3 - Use string interpolation with multiple variables and literal strings

--- part01/p01_module03/Program.cs
+++ part01/p01_module03/Program.cs
@@ -1,3 +1,4 @@
-﻿string firstName = "Bob";
-string message = $"Hello {firstName}!";
+﻿int version = 11;
+string updateText = "Update to Windows";
+string message = $"{updateText} {version}";
 Console.WriteLine(message);
