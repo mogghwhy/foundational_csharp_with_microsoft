4afddb134d4daba447c07db2f4ff89d8d7158ca0
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-10-03 11:30:11 +0300

    p1m4 - Challenge: Calculate Celsius given the current temperature in Fahrenheit

--- part01/p01_module04/Program.cs
+++ part01/p01_module04/Program.cs
@@ -1,6 +1,4 @@
-﻿int value = 1;
-value++;
-Console.WriteLine("First: " + value);
-Console.WriteLine($"Second: {value++}");
-Console.WriteLine("Third: " + value);
-Console.WriteLine("Fourth: " + (++value));
+﻿int fahrenheit = 94;
+// To convert temperatures in degrees Fahrenheit to Celsius, first subtract 32, then multiply by five ninths (5 / 9).
+// The temperature is 34.444444444444444444444444447 Celsius.
+Console.WriteLine($"The temperature is {(fahrenheit - 32) * (5m / 9)} Celsius.");
