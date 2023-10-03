060edfb652c7d4592c6246f51ae9ab03370eb58d
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-10-03 10:24:23 +0300

    p1m3 - Combine verbatim literals and string interpolation

--- part01/p01_module03/Program.cs
+++ part01/p01_module03/Program.cs
@@ -1,4 +1,2 @@
-﻿int version = 11;
-string updateText = "Update to Windows";
-string message = $"{updateText} {version}";
-Console.WriteLine(message);
+﻿string projectName = "First-Project";
+Console.WriteLine($@"C:\Output\{projectName}\Data");
