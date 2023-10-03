ac45e1d046f0155684d2f81c30379691d31efe9e
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-10-03 14:18:20 +0300

    p2m2 - overloaded dice roll

--- part02/p02_module02/Program.cs
+++ part02/p02_module02/Program.cs
@@ -1,6 +1,8 @@
-﻿int number = 7;
-string text = "seven";
+﻿Random dice = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
 
-Console.WriteLine(number);
-Console.WriteLine();
-Console.WriteLine(text);
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
