0cf06da4315ff86b133553d276ec551f4fbf839c
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-10-03 16:13:14 +0300

    p2m3 - Create decision logic with if statements

--- part02/p02_module03/Program.cs
+++ part02/p02_module03/Program.cs
@@ -6,7 +6,21 @@ int roll3 = dice.Next(1, 7);
 
 int total = roll1 + roll2 + roll3;
 
-if (total > 14)
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+{
+    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+    total += 2;
+}
+
+if ((roll1 == roll2) && (roll2 == roll3)) 
+{
+    Console.WriteLine("You rolled triples! +6 bonus to total!");
+    total += 6;
+}
+
+if (total >= 15)
 {
     Console.WriteLine("You win!");
 }
@@ -14,15 +28,4 @@ if (total > 14)
 if (total < 15)
 {
     Console.WriteLine("Sorry, you lose.");
-}
-
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-string message = "The quick brown fox jumps over the lazy dog.";
-bool result = message.Contains("dog");
-Console.WriteLine(result);
-
-if (message.Contains("fox"))
-{
-    Console.WriteLine("What does the fox say?");
 }
