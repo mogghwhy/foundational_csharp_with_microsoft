541505fa5f828076e8e0fc714b410debe6b9e7d4
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-10-03 14:17:43 +0300

    p2m2 - Overloaded methods

--- part02/p02_module02/Program.cs
+++ part02/p02_module02/Program.cs
@@ -1,3 +1,6 @@
-﻿Random dice = new Random();
-int roll = dice.Next(1, 7);
-Console.WriteLine(roll);
+﻿int number = 7;
+string text = "seven";
+
+Console.WriteLine(number);
+Console.WriteLine();
+Console.WriteLine(text);
