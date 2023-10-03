6075197003e23c188f9ee3307b674224d39a0a5a
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-10-03 11:23:03 +0300

    p1m4 - Increment and decrement

--- part01/p01_module04/Program.cs
+++ part01/p01_module04/Program.cs
@@ -1,4 +1,6 @@
-﻿int value1 = 3 + 4 * 5;
-int value2 = (3 + 4) * 5;
-Console.WriteLine(value1);
-Console.WriteLine(value2);
+﻿int value = 0;     // value is now 0.
+value = value + 5; // value is now 5.
+value += 5;        // value is now 10.
+value = 0;     // value is now 0.
+value = value + 1; // value is now 1.
+value++;           // value is now 2.
