0ce1fea13249fa81f302e0dace0472566f510df4
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-10-03 11:23:34 +0300

    p1m4 - Write code to increment and decrement a value

--- part01/p01_module04/Program.cs
+++ part01/p01_module04/Program.cs
@@ -1,6 +1,19 @@
-﻿int value = 0;     // value is now 0.
-value = value + 5; // value is now 5.
-value += 5;        // value is now 10.
-value = 0;     // value is now 0.
-value = value + 1; // value is now 1.
-value++;           // value is now 2.
+﻿int value = 1;
+
+value = value + 1;
+Console.WriteLine("First increment: " + value);
+
+value += 1;
+Console.WriteLine("Second increment: " + value);
+
+value++;
+Console.WriteLine("Third increment: " + value);
+
+value = value - 1;
+Console.WriteLine("First decrement: " + value);
+
+value -= 1;
+Console.WriteLine("Second decrement: " + value);
+
+value--;
+Console.WriteLine("Third decrement: " + value);
