8f9f72b1f78a6bc8428644b5dc6039ab9f390b52
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-10-03 16:14:39 +0300

    p2m3 - use if else

--- part02/p02_module03/Program.cs
+++ part02/p02_module03/Program.cs
@@ -14,7 +14,7 @@ if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
     total += 2;
 }
 
-if ((roll1 == roll2) && (roll2 == roll3)) 
+if ((roll1 == roll2) && (roll2 == roll3))
 {
     Console.WriteLine("You rolled triples! +6 bonus to total!");
     total += 6;
@@ -25,7 +25,7 @@ if (total >= 15)
     Console.WriteLine("You win!");
 }
 
-if (total < 15)
+else
 {
     Console.WriteLine("Sorry, you lose.");
 }
