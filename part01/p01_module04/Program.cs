c5076ca73f8bab605bdb4f9635813dec7e247c37
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-10-03 11:21:16 +0300

    p1m4 - Write code to determine the remainder after integer division

--- part01/p01_module04/Program.cs
+++ part01/p01_module04/Program.cs
@@ -1,4 +1,2 @@
-﻿int first = 7;
-int second = 5;
-decimal quotient = (decimal)first / (decimal)second;
-Console.WriteLine(quotient);
+﻿Console.WriteLine($"Modulus of 200 / 5 : {200 % 5}");
+Console.WriteLine($"Modulus of 7 / 5 : {7 % 5}");
