a3516d57fe0b9531cc71fa3bdda02442cddc7946
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-10-03 10:10:41 +0300

    p1 m3 - Format output using character escape sequences

--- part01/p01_module03/Program.cs
+++ part01/p01_module03/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.WriteLine("Hello\nWorld!");
-Console.WriteLine("Hello\tWorld!");
-Console.WriteLine("Hello \"World\"!");
-Console.WriteLine("c:\\source\\repos");
+﻿Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
