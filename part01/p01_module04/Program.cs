44b51d6fb432b237d8cff89366db9166bff48c87
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-10-03 11:17:59 +0300

    p1m4 - Write code to perform addition, subtraction, multiplication, and division with integers

--- part01/p01_module04/Program.cs
+++ part01/p01_module04/Program.cs
@@ -1,3 +1,9 @@
-﻿string firstName = "Bob";
-int widgetsSold = 7;
-Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+﻿int sum = 7 + 5;
+int difference = 7 - 5;
+int product = 7 * 5;
+int quotient = 7 / 5;
+
+Console.WriteLine("Sum: " + sum);
+Console.WriteLine("Difference: " + difference);
+Console.WriteLine("Product: " + product);
+Console.WriteLine("Quotient: " + quotient);
