768c7c538c7a90720001c5edbe8498dd16ce0e19
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-10-04 11:15:43 +0300

    p2m5 - Variable name examples

--- part02/p02_module05/Program.cs
+++ part02/p02_module05/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿char userOption;
+
+int gameScore;
+
+float particlesPerMillion;
+
+bool processedCustomer;
