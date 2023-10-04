40cfb49a94ada39057cda9ae39f4018762928707
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-10-04 11:46:19 +0300

    p2m5 - add whitespace to increase code readability

--- part02/p02_module05/Program.cs
+++ part02/p02_module05/Program.cs
@@ -1,11 +1,7 @@
 ﻿// Example 1:
-Console
-.
-WriteLine
-(
-"Hello Example 1!"
-)
-;
+Console.WriteLine("Hello Example 1!");
 
 // Example 2:
-string firstWord="Hello";string lastWord="Example 2";Console.WriteLine(firstWord+" "+lastWord+"!");
+string firstWord = "Hello";
+string lastWord = "Example 2";
+Console.WriteLine(firstWord + " " + lastWord + "!");
