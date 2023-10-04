d0f564854853535adb8a6fff38876019d3b3a1c4
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-10-04 11:52:17 +0300

    p2m5 - solution to the style guide challenge

--- part02/p02_module05/Program.cs
+++ part02/p02_module05/Program.cs
@@ -1,13 +1,21 @@
-﻿string str = "The quick brown fox jumps over the lazy dog.";
-// convert the message into a char array
+﻿/*
+converts the message into a char array, then reverses the chars.
+counts the o's, converts it back to a string.
+prints the message out
+*/
+string str = "The quick brown fox jumps over the lazy dog.";
+
 char[] charMessage = str.ToCharArray();
-// Reverse the chars
+
 Array.Reverse(charMessage);
 int x = 0;
-// count the o's
-foreach (char i in charMessage) { if (i == 'o') { x++; } }
-// convert it back to a string
+
+foreach (char i in charMessage)
+{
+    if (i == 'o') { x++; }
+}
+
 string new_message = new String(charMessage);
-// print it out
+
 Console.WriteLine(new_message);
 Console.WriteLine($"'o' appears {x} times.");
