105492f1c33c8912bca93b18062a30412c806b3f
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-11-22 08:16:24 +0200

    p2m6 - Update the nested loop to calculate all student scores

--- part02/p02_module06/Program.cs
+++ part02/p02_module06/Program.cs
@@ -7,28 +7,37 @@ int currentAssignments = 5;
 string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 
 int[] sophiaScores = new int[] { 90, 86, 87, 98, 100 };
-int[] andrewScores = new int[] {92, 89, 81, 96, 90};
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90 };
 int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
 int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
+int[] studentScores = new int[10];
 
 foreach (var name in studentNames)
 {
-    if (name == "Sophia")
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+    int sophiaSum = 0;
+
+    decimal sophiaScore;
+
+    foreach (int score in sophiaScores)
     {
-        int sophiaSum = 0;
-
-        decimal sophiaScore;
-
-        foreach (int score in sophiaScores)
-        {
-            sophiaSum += score;
-        }
-        sophiaScore = (decimal)sophiaSum / currentAssignments;
-
-        Console.WriteLine("Student\t\tGrade\n");
-        Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+        sophiaSum += score;
     }
+    sophiaScore = (decimal)sophiaSum / currentAssignments;
+
+    Console.WriteLine("Student\t\tGrade\n");
+    Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
 }
 
 Console.WriteLine("Press the Enter key to continue");
