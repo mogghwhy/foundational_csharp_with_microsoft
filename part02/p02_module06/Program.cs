9c53417584d521bc130107ee04c82d0da55fcfe5
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-11-22 07:55:53 +0200

    p2m6 - Calculate Sophia's score inside the outer names loop

--- part02/p02_module06/Program.cs
+++ part02/p02_module06/Program.cs
@@ -11,18 +11,25 @@ int[] andrewScores = new int[] {92, 89, 81, 96, 90};
 int[] emmaScores = new int[] { 90, 85, 87, 98, 68 };
 int[] loganScores = new int[] { 90, 95, 87, 88, 96 };
 
-int sophiaSum = 0;
 
-decimal sophiaScore;
-
-foreach (int score in sophiaScores)
+foreach (var name in studentNames)
 {
-    sophiaSum += score;
-}
-sophiaScore = (decimal)sophiaSum / currentAssignments;
+    if (name == "Sophia")
+    {
+        int sophiaSum = 0;
 
-Console.WriteLine("Student\t\tGrade\n");
-Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+        decimal sophiaScore;
+
+        foreach (int score in sophiaScores)
+        {
+            sophiaSum += score;
+        }
+        sophiaScore = (decimal)sophiaSum / currentAssignments;
+
+        Console.WriteLine("Student\t\tGrade\n");
+        Console.WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
+    }
+}
 
 Console.WriteLine("Press the Enter key to continue");
 Console.ReadLine();
