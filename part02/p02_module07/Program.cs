aa37d87420c83e0084ead21afb3b9fcb6f461e6f
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2023-11-24 08:53:07 +0200

    p2m7 - adjusted header values

--- part02/p02_module07/Program.cs
+++ part02/p02_module07/Program.cs
@@ -33,7 +33,7 @@ string currentStudentLetterGrade = "";
 
 // display the header row for scores/grades
 Console.Clear();
-Console.WriteLine("Student\t\tGrade\tLetter Grade\n");
+Console.WriteLine("Student\t\tExam Score\tOverall Grade\tExtra Credit");
 
 /*
 The outer foreach loop is used to:
