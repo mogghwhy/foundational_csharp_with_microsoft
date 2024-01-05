2e5f1507ac86cbe399a5e74cae6b7ccec691e6c8
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2024-01-05 13:20:06 +0200

    p3m1 - Use the Logical Negation operator

--- part03/p03_module01/Program.cs
+++ part03/p03_module01/Program.cs
@@ -3,6 +3,10 @@
 // Console.WriteLine(1 >= 1);
 // Console.WriteLine(1 <= 1);
 
-string pangram = "The quick brown cow jumps over the lazy dog.";
-Console.WriteLine(pangram.Contains("fox"));
-Console.WriteLine(pangram.Contains("cow"));
+// string pangram = "The quick brown cow jumps over the lazy dog.";
+// Console.WriteLine(pangram.Contains("fox"));
+// Console.WriteLine(pangram.Contains("cow"));
+
+string pangram = "The quick brown fox jumps over the lazy dog.";
+Console.WriteLine(!pangram.Contains("fox"));
+Console.WriteLine(!pangram.Contains("cow"));
