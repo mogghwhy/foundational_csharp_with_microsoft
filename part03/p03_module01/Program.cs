e2939e670640db686834d45261e7fe414fd535ea
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2024-01-05 13:19:19 +0200

    p3m1 - Use a method that returns a Boolean

--- part03/p03_module01/Program.cs
+++ part03/p03_module01/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.WriteLine(1 > 2);
-Console.WriteLine(1 < 2);
-Console.WriteLine(1 >= 1);
-Console.WriteLine(1 <= 1);
+﻿// Console.WriteLine(1 > 2);
+// Console.WriteLine(1 < 2);
+// Console.WriteLine(1 >= 1);
+// Console.WriteLine(1 <= 1);
+
+string pangram = "The quick brown cow jumps over the lazy dog.";
+Console.WriteLine(pangram.Contains("fox"));
+Console.WriteLine(pangram.Contains("cow"));
