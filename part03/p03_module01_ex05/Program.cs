20e720e3512f575eaa416afc0b785cd84c674bad
mogghwhy <112501265+mogghwhy@users.noreply.github.com>
2024-06-28 10:20:59 +0300

    p3m1ex5 - added solution for the exercise

--- part03/p03_module01_ex05/Program.cs
+++ part03/p03_module01_ex05/Program.cs
@@ -1,2 +1,22 @@
 ﻿string permission = "Admin|Manager";
 int level = 55;
+
+if (permission.Contains("Admin")) {
+    if (level > 55) {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else if (level <= 55) {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager")) {
+    if (level >= 20) {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else if (level < 20) {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else {
+    Console.WriteLine("You do not have sufficient privileges.");
+}
